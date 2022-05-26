2690105f2801c09210d979ef9aede6fffe5ef10f
Habeba Ahmed <habebaahmed101@gmail.com>
2022-05-26 02:29:05 +0200

    the first two sections

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Program // Note: actual namespace depends on the project name.
+{
+    
+        // class ArrayCal{
+        // public void MaxMinArr(){
+        //     int max,min;
+        //     int size = int.Parse(Console.ReadLine());
+        //     int [] array1 = new int[size];
+        //     for(int i =0;i<size;i++){
+        //         array1[i]= int.Parse(Console.ReadLine());
+
+        //     }
+        //     max= array1[0];
+        //     min= array1[0];
+        //     for(int j=1;j<size;j++){
+        //         if(array1[j]>max){
+        //             max=array1[j];
+        //         }
+        //         if(array1[j]<min){
+        //             min=array1[j];
+        //         }
+        //     }
+        //     Console.WriteLine("max is"+ max);
+        //     Console.WriteLine("min is "+min);
+        // }
+
+        // public bool PosNeg(int num){
+        //     bool posNeg=false;
+        //     if(num>0){
+        //         posNeg=true;
+        //     }
+        //     if(num<0){
+        //         posNeg=false;
+        //     }
+        //     return posNeg;
+        // }
+        // public int Power(int x, int y){
+        //     int pow=1;
+        //     for(int i=0;i<y;i++){
+        //         pow*=x;
+        //     }
+        //     return pow;
+        // }
+        // public bool divisable(int x,int y){
+        //     bool res= false;
+        //     if (x%y==0) res= true;
+        //     else res = false;
+        //     return res;
+        // }
+        // public void factOne(int num){
+        //     double fact=1.0;
+        //     double factone=0.0;
+        //     for(int i =1; i<=num;i++){
+        //         fact*=i;
+        //         factone+=(1/fact);
+        //     }
+        //     System.Console.WriteLine(factone);
+        // }
+        
+        // }
+     class Program
+    {
+        // public int Sum (){
+        //       Console.WriteLine("enter size");
+        //     int sum =0;
+        //     int size;
+        //     size= int.Parse(Console.ReadLine());
+        //     int [] array1 = new int [size] ;
+        //     for(int i =0;i<size;i++){
+        //         array1[i]= int.Parse(Console.ReadLine());
+        //         sum +=array1[i];
+
+        //     }
+        //     Console.WriteLine(sum);
+        //     return sum ;
+        // }
+
+
+        static void Main(string[] args)
+        { 
+            // Program p =new Program();
+            // int sum1 = p.Sum();
+            // ArrayCal arr = new ArrayCal();
+            // arr.MaxMinArr();
+        //    System.Console.WriteLine(arr.PosNeg(5));
+        //    System.Console.WriteLine(arr.PosNeg(-100));
+                //  Console.WriteLine(arr.Power(5,2));
+                //  Console.WriteLine(arr.Power(10,3));
+                // Console.WriteLine(arr.Power(2,6));
+                
+                // Console.WriteLine(arr.divisable(5,2));
+                //  Console.WriteLine(arr.divisable(10,3));
+                // Console.WriteLine(arr.divisable(6,2));
+                
+                //    arr.factOne(4);
+                
+
+            
+            Console.ReadKey();       
+        }
+    }
+}
